935f84558458d079ebddf5053a202048706ad532
Ilya <leynsboro@gmail.com>
2022-10-18 13:05:47 +0300

    Initial Commit

---
+++ TR VPN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TR VPN
+//
+//  Created by Илья Гусаров on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
