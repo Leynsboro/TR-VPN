d23403387ffe1119518648ed0ac139441abcaff5
Ilya <leynsboro@gmail.com>
2022-11-24 19:07:23 +0300

    Created color constants

---
+++ TR VPN/Extentions/Extention+UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  Extention+UIColor.swift
+//  TR VPN
+//
+//  Created by Илья Гусаров on 24.11.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let firstButtonColor = UIColor(red: 73/255, green: 137/255, blue: 183/255, alpha: 1)
+    static let secondButtonColor = UIColor(red: 0/255, green: 106/255, blue: 183/255, alpha: 1)
+    static let mainBackgroundColor = UIColor(red: 0/255, green: 42/255, blue: 72/255, alpha: 1)
+    static let tableViewColor = UIColor(red: 15/255, green: 76/255, blue: 120/255, alpha: 1)
+}
